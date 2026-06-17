09868214de913da8f570b6b1bde571374778ce8f
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-06-17 14:06:35 +0500

    Add player marker logic on game

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('is Ok?')
+const buttons = document.querySelectorAll('button');
+
+let check = true;
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', () => {
+        if(check) {
+            button.textContent = 'X';
+            check = false;
+            button.disabled = true;
+            button.style.color = 'darkgreen';
+        } else if(check === false){
+            button.textContent = 'O';
+            button.disabled = true;
+            button.style.color = 'darkred';
+            check = true;
+        }
+    });
+});
